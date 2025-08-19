7f11193b390d610e9929e4c6e1de3199907093fe
James Shen <jamesjieranshen@gmail.com>
2025-08-19 11:37:05 -0400

    Allow all fields in RunProvider to be configurable

--- src/RunProvider.cc
+++ src/RunProvider.cc
@@ -5,7 +5,6 @@
 
 #include <CLI/CLI.hpp>
 #include <TChainElement.h>
-#include <locale>
 
 namespace SnopAnalysis {
 void
@@ -13,17 +12,27 @@ RunProvider::Configure(const nlohmann::json& config) {
   InputProvider::Configure(config);
   // format: Analysis15_bMR_r0000363272_s004_p008.ntuple.root
   std::string directory = config.value("directory", "");
-  std::string module_name = config.value("module_name", "Analysis15_bMR");
+  std::string module_name = config.value("module_name", "");
   std::string tree_name = config.value("tree_name", "output");
   int run_number = config.value("run_number", -9999);
-  if (run_number <= 0) {
+  CLI::App app{"run provider parser"};
+  app.allow_extras();
+  if (run_number == -9999) {
     Logger::Info("No run number provided, using attempting to read command line.");
-    CLI::App app{"run_number_parser"};
     app.add_option("-r,--run", run_number, "run number");
-    app.allow_extras();
-    app.parse(GetContext()->fCmdLine);
-    Logger::Info("Using run number: {}", run_number);
   }
+  if (directory.empty()) {
+    Logger::Info("No directory provided, using attempting to read command line.");
+    app.add_option("-d,--directory", directory, "directory containing the ntuple files");
+  }
+  if (module_name.empty()) {
+    Logger::Info("No module name provided, using attempting to read command line.");
+    app.add_option("-m,--module", module_name, "module name");
+  }
+  app.parse(GetContext()->fCmdLine);
+  Logger::Info("Using directory: {}", directory);
+  Logger::Info("Using module name: {}", module_name);
+  Logger::Info("Using run number: {}", run_number);
   nlohmann::json cfg = {{"tree_name", tree_name},
                         {"files", {std::format("{}/{}_r{:010d}_*.ntuple.root", directory, module_name, run_number)}},
                         {"sort", true}};
